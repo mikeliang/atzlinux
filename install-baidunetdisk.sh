0cb56440e41caf9d1859889a2eeee05194ac8e42
肖盛文 <atzlinux@yeah.net>
2019-10-04 23:26:56 +0800

    增加百度网盘的相关脚本

---
+++ install-baidunetdisk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd `dirname $0`; pwd
+echo '即将开始安装 ...'
+apt -y install ./baidunetdisk/baidunetdisk_linux_2.0.2.deb
--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,10 @@ echo "开始卸载 Debian 中文套件 ... ：-（..."
 echo "开始卸载  qq ..."
 ./qq/uninstall-qq.sh
 
-
 echo "开始卸载  wine32 ..."
 ./deepin-wine32/uninstall-wine32.sh
 
+echo "开始卸载 baidunetdisk ..."
+./baidunetdisk/uninstall-baidunetdisk.sh
+
 echo "开始卸载 ..."
-echo " ..."
