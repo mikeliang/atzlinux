97b2d1a51d9ed0a1076518911fa1507cf341e8d7
肖盛文 <atzlinux@yeah.net>
2019-10-05 19:25:26 +0800

    明确指示 dpkg 增加 i386 架构

--- install-wine32.sh
+++ install-wine32.sh
@@ -4,6 +4,7 @@ echo '即将开始安装  ...'
 ./install-deepin-keyring.sh  
 echo '增加 deepin 软件包安装仓库'
 apt-add-repository 'deb http://mirrors.huaweicloud.com/deepin stable main non-free contrib'
+dpkg --add-architecture i386
 apt update
 apt -y install deepin-wine deepin-wine-helper:i386 deepin-wine-plugin deepin-wine-plugin-virtual deepin-wine-uninstaller:i386 deepin-wine32-preloader:i386
 echo '安装完成，正在自动安装依赖...'
