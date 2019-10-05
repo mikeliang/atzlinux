3cc260e58db1af679f25754b217521c1414ad238
肖盛文 <atzlinux@yeah.net>
2019-10-05 12:18:34 +0800

    add wechat install

--- install-wechat.sh
+++ install-wechat.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd wechat
 cd `dirname $0`; pwd
 echo '即将开始安装 ...'
-apt -y install ./wechat/electronic-wechat_2.0.1_amd64.deb
+wget -c -O electronic-wechat_2.0.1_amd64.deb  http://archive.ubuntukylin.com/ubuntukylin/pool/main/e/electronic-wechat/electronic-wechat_2.0.1_amd64.deb 
+
+apt -y install ./electronic-wechat_2.0.1_amd64.deb
+
+cd ..
