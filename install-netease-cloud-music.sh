8384c6cb2c3486f925d92d6e4e2d0e4090d6a36b
肖盛文 <atzlinux@yeah.net>
2019-10-05 13:22:54 +0800

    modify: download .deb from Internet

--- install-netease-cloud-music.sh
+++ install-netease-cloud-music.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cd netease-cloud-music
 cd `dirname $0`; pwd
 echo '即将开始安装 ...'
-apt -y install ./netease-cloud-music/netease-cloud-music_1.2.1_amd64_ubuntu_20190428.deb
+wget -c -O netease-cloud-music_1.2.1_amd64_ubuntu_20190428.deb http://d1.music.126.net/dmusic/netease-cloud-music_1.2.1_amd64_ubuntu_20190428.deb
+
+apt -y install ./netease-cloud-music_1.2.1_amd64_ubuntu_20190428.deb
+
+cd ..
