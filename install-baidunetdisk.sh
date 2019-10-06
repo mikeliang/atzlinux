8a6b455cc87381a05771bbba49f0d775b47fe657
肖盛文 <atzlinux@yeah.net>
2019-10-06 11:57:20 +0800

    安装百度网盘需要用到  update-desktop-database 命令 该命令在 desktop-file-utils 包内

--- install-baidunetdisk.sh
+++ install-baidunetdisk.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd `dirname $0`; pwd
 echo '即将开始安装 ...'
+
+# 安装 update-desktop-database 命令
+apt -y install desktop-file-utils
+
 apt -y install ./baidunetdisk/baidunetdisk_linux_2.0.2.deb
