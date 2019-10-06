d41b1ff5e7bc9bf8500a6028e0d38827a8550cad
肖盛文 <atzlinux@yeah.net>
2019-10-06 17:45:06 +0800

    更新安装脚本，使用添加清理 deepin 仓库脚本

--- install-wine32.sh
+++ install-wine32.sh
@@ -2,16 +2,9 @@
 cd `dirname $0`; pwd
 echo '即将开始安装  ...'
 ./install-deepin-keyring.sh  
-echo '增加 deepin 软件包安装仓库'
-apt -y install software-properties-common
-apt-add-repository 'deb http://mirrors.huaweicloud.com/deepin stable main non-free contrib'
-dpkg --add-architecture i386
-apt update
+./deepin/add-deepin-repo.sh
 apt -y install deepin-wine deepin-wine-helper:i386 deepin-wine-plugin deepin-wine-plugin-virtual deepin-wine-uninstaller:i386 deepin-wine32-preloader:i386
 echo '安装完成，正在自动安装依赖...'
 apt install -f
-echo '清理 deepin 软件包安装仓库'
-apt-add-repository -r 'deb http://mirrors.huaweicloud.com/deepin stable main non-free contrib'
 ./deepin/uninstall-deepin-keyring.sh
-rm -fv /var/lib/apt/lists/*deepin*
-apt update
+./deepin/clear-deepin-repo.sh
