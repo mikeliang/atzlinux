88f9abf365b743e88355e0de3321b342e5a3d099
肖盛文 <atzlinux@yeah.net>
2019-10-06 12:27:08 +0800

    增加强制给有道词典打补丁的选项，避免安装过程中断

--- install-youdao-dict.sh
+++ install-youdao-dict.sh
@@ -6,6 +6,6 @@ wget -c -O youdao-dict_1.1.0-0-deepin_amd64.deb http://codown.youdao.com/cidian/
 
 apt -y install ./youdao-dict_1.1.0-0-deepin_amd64.deb
 
-patch -p 1 /usr/share/youdao-dict/dae/ewmh.py < youdao-dict.patch
+patch -f -p 1 /usr/share/youdao-dict/dae/ewmh.py < youdao-dict.patch
 
 cd ..
