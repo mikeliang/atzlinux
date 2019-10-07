2c478c7a4ddc32b536bdbe238326399f7bdd88e5
肖盛文 <atzlinux@yeah.net>
2019-10-07 11:43:53 +0800

    自动答复 y，自行安装所有依赖包

--- install-wine32.sh
+++ install-wine32.sh
@@ -5,6 +5,6 @@ echo '即将开始安装  ...'
 ./deepin/add-deepin-repo.sh
 apt -y install deepin-wine deepin-wine-helper:i386 deepin-wine-plugin deepin-wine-plugin-virtual deepin-wine-uninstaller:i386 deepin-wine32-preloader:i386
 echo '安装完成，正在自动安装依赖...'
-apt install -f
+apt -f -y install
 ./deepin/uninstall-deepin-keyring.sh
 ./deepin/clear-deepin-repo.sh
