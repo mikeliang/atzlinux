6d2a8fa7fa19ee3ed80dbddb38c046a96af33a87
肖盛文 <atzlinux@yeah.net>
2019-10-10 12:24:01 +0800

    QQ 软件包升级更新脚本

---
+++ update-qq.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd `dirname $0`; pwd
+echo '即将开始  ...'
+./install-deepin-keyring.sh  
+./deepin/add-deepin-repo.sh
+apt -y install deepin.com.qq.im:i386
+echo '安装完成，正在自动安装依赖...'
+apt -f -y install
+./deepin/uninstall-deepin-keyring.sh
+./deepin/clear-deepin-repo.sh
