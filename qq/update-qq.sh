8dc70a935fe8d24938eb611df19a64fbed0aad05
肖盛文 <atzlinux@yeah.net>
2019-10-10 14:20:55 +0800

    增加更新说明和脚本

--- qq/update-qq.sh
+++ qq/update-qq.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd `dirname $0`; pwd
 echo '即将开始  ...'
-./install-deepin-keyring.sh  
-./deepin/add-deepin-repo.sh
+../install-deepin-keyring.sh  
+../deepin/add-deepin-repo.sh
 apt -y install deepin.com.qq.im:i386
 echo '安装完成，正在自动安装依赖...'
 apt -f -y install
-./deepin/uninstall-deepin-keyring.sh
-./deepin/clear-deepin-repo.sh
+../deepin/uninstall-deepin-keyring.sh
+../deepin/clear-deepin-repo.sh
