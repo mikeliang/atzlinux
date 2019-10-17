41b8b75e71e5c51c66604a967eb62e933084ef7f
肖盛文 <atzlinux@yeah.net>
2019-10-17 18:11:12 +0800

    bug fix ,no need dirname

--- i386/install-all-single-script.sh
+++ i386/install-all-single-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt -y install wget
-wget -c -O ./debian-cn-keyring/debian-cn-archive-keyring_2019.10.15_all.deb http://118.24.9.73/debian/pool/main/d/debian-cn-archive-keyring/debian-cn-archive-keyring_2019.10.15_all.deb
-apt -y install ./debian-cn-keyring/debian-cn-archive-keyring_2019.10.15_all.deb 
+wget -c -O ./debian-cn-archive-keyring_2019.10.15_all.deb http://118.24.9.73/debian/pool/main/d/debian-cn-archive-keyring/debian-cn-archive-keyring_2019.10.15_all.deb
+apt -y install ./debian-cn-archive-keyring_2019.10.15_all.deb 
 apt -y install software-properties-common
 apt-add-repository 'deb http://118.24.9.73/debian/ stable main non-free'
 dpkg --add-architecture i386
