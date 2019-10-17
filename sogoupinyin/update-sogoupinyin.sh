0976dddc0062e09267a2024d8c6c38d90bec3c42
肖盛文 <atzlinux@yeah.net>
2019-10-17 11:19:04 +0800

    取消有道补丁文件，搜狗拼音更新文件

--- sogoupinyin/update-sogoupinyin.sh
+++
@@ -1,8 +0,0 @@
-apt -y install software-properties-common
-apt-add-repository 'deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main'
-apt update
-apt -y install sogoupinyin
-apt-add-repository -r 'deb http://archive.ubuntukylin.com:10006/ubuntukylin xenial main'
-
-rm -fv /var/lib/apt/lists/*ubuntukylin* 
-apt update
