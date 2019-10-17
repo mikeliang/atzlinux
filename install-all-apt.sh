4b46820f8a5d75e8ef204011927c3aae67c93e61
肖盛文 <atzlinux@yeah.net>
2019-10-17 14:50:11 +0800

    增加用 apt 一次性安装所有软件包的脚本

---
+++ install-all-apt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "开始安装 Debian-cn 中文套件 ..."
+
+./install-debian-cn-repo.sh
+
+apt -y install  \
+baidunetdisk \
+netease-cloud-music \
+deepin.com.qq.im \
+sogoupinyin \
+electronic-wechat \
+youdao-dict \
+wps-office wps-office-fonts
--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 cd `dirname $0`; pwd
 echo "开始安装 Debian 中文套件 ..."
 
-echo "开始安装 wine32 ..."
-./install-wine32.sh
-
 echo "开始安装 qq ..."
 ./install-qq.sh
 
