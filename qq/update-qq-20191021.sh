a3096da744e81d9dd91ed904f305865746833b7f
肖盛文 <atzlinux@yeah.net>
2019-10-21 15:57:53 +0800

    同步更新 更新脚本

--- qq/update-qq-20191021.sh
+++ qq/update-qq-20191021.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update
-apt -y install xsettingsd
+apt -y install xfce4-settings
 apt -y install libcanberra-gtk-module
 apt -y install libcanberra-gtk-module:i386
 sed -i '11 i\xfsettingsd --display :0.0 & \n' /opt/deepinwine/apps/Deepin-QQ/run.sh
