ae32182adfbfdc780d5775002f56d05d44cf648b
肖盛文 <atzlinux@yeah.net>
2019-10-23 21:40:31 +0800

    逐个安装软件，适应网络带宽小的情况

--- install-all-single-script.sh
+++ install-all-single-script.sh
@@ -10,15 +10,14 @@ apt -y install xdg-utils
 apt -y install xfce4-settings
 apt -y install libcanberra-gtk-module
 apt -y install libcanberra-gtk-module:i386
-apt -y install  \
-baidunetdisk \
-netease-cloud-music \
-deepin.com.qq.im \
-sogoupinyin \
-electronic-wechat \
-youdao-dict \
-wps-office wps-office-fonts
+apt -y install electronic-wechat
+apt -y install deepin.com.qq.im
+apt -y install sogoupinyin
+apt -y install debian-cn-fonts
+apt -y install baidunetdisk
+apt -y install netease-cloud-music
+apt -y install youdao-dict
+apt -y install wps-office wps-office-fonts
 sed -i '11 i\xfsettingsd --display :0.0 & \n' /opt/deepinwine/apps/Deepin-QQ/run.sh
 rm -f /etc/apt/sources.list.d/sogoupinyin.list
-apt -y install debian-cn-fonts
 echo "安装成功，请退出当前登录，重新登录，让安装生效。"
