766f6a8bed4fda9a4150423f6ce1ffba887d83bf
肖盛文 <atzlinux@yeah.net>
2019-11-03 09:57:29 +0800

    构建脚本更新配置文件名

--- isodvd/debian-cn.build.sh
+++
@@ -1,1 +0,0 @@
-build-simple-cdd --debug --dvd --proposed-updates --dist buster --locale zh_CN --keyboard us --security-mirror https://mirrors.huaweicloud.com/debian-security/ --debian-mirror https://mirrors.huaweicloud.com/debian/  -b debian-cn.build
