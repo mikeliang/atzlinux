d4a3e79d8683d2220999deaf45c4b02bb0297db8
肖盛文 <atzlinux@yeah.net>
2019-11-07 17:14:10 +0800

    增加 amd64 自动化安装配置文件和构建脚本

---
+++ isodvd/amd64-xall.build.sh
@@ -0,0 +1,6 @@
+build-simple-cdd --debug --dvd --proposed-updates --dist buster --locale zh_CN --keyboard us  \
+--security-mirror \
+http://mirrors.tencentyun.com/debian-security/ \
+--debian-mirror \
+http://mirrors.tencentyun.com/debian/  \
+-b amd64-xall.build
