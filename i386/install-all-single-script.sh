f01cb7fab3c391a1038dfd7f9fbf7556fb15a158
肖盛文 <atzlinux@yeah.net>
2019-11-10 10:09:41 +0800

    i386 安装使用 CDN 作为 apt 源

--- i386/install-all-single-script.sh
+++ i386/install-all-single-script.sh
@@ -3,7 +3,8 @@ apt -y install wget
 wget -c -O ./debian-cn-archive-keyring_2019.10.15_all.deb http://118.24.9.73/debian/pool/main/d/debian-cn-archive-keyring/debian-cn-archive-keyring_2019.10.15_all.deb
 apt -y install ./debian-cn-archive-keyring_2019.10.15_all.deb 
 apt -y install software-properties-common
-apt-add-repository 'deb http://118.24.9.73/debian/ stable main non-free'
+#apt-add-repository 'deb http://118.24.9.73/debian/ stable main non-free'
+apt-add-repository 'deb http://debian-cn.lilishare.com/debian/ stable main non-free'
 dpkg --add-architecture i386
 apt update
 apt -y install xdg-utils
