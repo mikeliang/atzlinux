e11afbfa000ea849f3158d3898354ef1d54568ba
肖盛文 <atzlinux@yeah.net>
2019-12-15 22:26:31 +0800

    增加 all-txt-tech 构建脚步

---
+++ isodvd/txy-all-txt-tech.build.sh
@@ -0,0 +1,7 @@
+build-simple-cdd --debug --dvd --proposed-updates --dist buster --locale zh_CN --keyboard us  \
+--security-mirror \
+http://mirrors.tencentyun.com/debian-security/ \
+--debian-mirror \
+http://mirrors.tencentyun.com/debian/  \
+-b all-txt-tech.build \
+-a debian-cn-all-txt-tech
