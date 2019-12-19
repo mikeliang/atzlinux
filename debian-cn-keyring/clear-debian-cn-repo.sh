60556e264c712bd5d10cb670fa73d9bcb2a80dd2
肖盛文 <atzlinux@yeah.net>
2019-12-19 10:37:42 +0800

    修改 apt 源到 https://www.atzlinux.com；更新 debian-cn-archive-keyring 下载地址

--- debian-cn-keyring/clear-debian-cn-repo.sh
+++ debian-cn-keyring/clear-debian-cn-repo.sh
@@ -1,6 +1,8 @@
 echo '清理 debian-cn 软件包安装仓库'
 apt-add-repository -r 'deb http://118.24.9.73/debian/ stable main non-free'
 apt-add-repository -r 'deb http://debian-cn.lilishare.com/debian/ stable main non-free'
+apt-add-repository -r 'deb https://apt.atzlinux.com/debian buster  main contrib non-free'
 rm -fv /var/lib/apt/lists/*118.24.9.73*
 rm -fv /var/lib/apt/lists/*debian-cn.lilishare.com*
+rm -fv /var/lib/apt/lists/*apt.atzlinux.com*
 apt update
