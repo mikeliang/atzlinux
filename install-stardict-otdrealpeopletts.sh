9e2180df81800d164537da5477b7d4f2bf171f98
肖盛文 <atzlinux@yeah.net>
2020-01-31 09:43:13 +0800

    使用铜豌豆软件源来安装星际译王声音包

--- install-stardict-otdrealpeopletts.sh
+++ install-stardict-otdrealpeopletts.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 apt -y install wget
-apt -y install software-properties-common
-wget -c -O debian-cn-archive-keyring_2019.10.15_all.deb https://www.atzlinux.com/debian/pool/main/d/debian-cn-archive-keyring/debian-cn-archive-keyring_2019.10.15_all.deb
-apt -y install ./debian-cn-archive-keyring_2019.10.15_all.deb
-apt-add-repository 'deb https://www.atzlinux.com/debian/ stable main non-free'
-apt-add-repository "deb https://mirrors.huaweicloud.com/debian/ unstable main"
-apt -y install sudo
+wget -c -O atzlinux-archive-keyring_lastest_all.deb https://www.atzlinux.com/debian/pool/main/a/atzlinux-archive-keyring/atzlinux-archive-keyring_lastest_all.deb
+apt -y install ./atzlinux-archive-keyring_lastest_all.deb
+dpkg --add-architecture i386
 apt update
+apt -y install sudo
 apt -y install stardict-otdrealpeopletts
-apt-add-repository -r "deb https://mirrors.huaweicloud.com/debian/ unstable main"
