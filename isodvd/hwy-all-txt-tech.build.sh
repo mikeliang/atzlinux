553dd881df8aa81b6109d803f5bc8ea6ec87dbc8
肖盛文 <atzlinux@yeah.net>
2020-02-13 11:37:04 +0800

    增加纯文本技术版构建脚本

---
+++ isodvd/hwy-all-txt-tech.build.sh
@@ -0,0 +1,9 @@
+build-simple-cdd --debug --dvd --proposed-updates --dist buster --locale zh_CN --keyboard us  \
+--security-mirror \
+http://mirrors.huaweicloud.com/debian-security/ \
+--debian-mirror \
+http://mirrors.huaweicloud.com/debian/  \
+--backports-mirror \
+http://mirrors.huaweicloud.com/debian \
+-b all-text-tech.build \
+-a debian-cn-all-text-tech
