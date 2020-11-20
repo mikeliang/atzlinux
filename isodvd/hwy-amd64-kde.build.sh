95f3bbc41eb8f10c3ebf1d75d17f12cfc6b6026c
肖盛文 <atzlinux@yeah.net>
2020-11-20 21:26:09 +0800

    增加 kde iso 构建脚本

---
+++ isodvd/hwy-amd64-kde.build.sh
@@ -0,0 +1,7 @@
+build-simple-cdd --debug --dvd --dist buster --locale zh_CN --keyboard us  \
+--security-mirror \
+http://mirrors.huaweicloud.com/debian-security/ \
+--debian-mirror \
+http://mirrors.huaweicloud.com/debian/  \
+-b amd64-kde.build \
+-a atzlinux-amd64-kde
---
+++ isodvd/txy-amd64-kde.build.sh
@@ -0,0 +1,7 @@
+build-simple-cdd --debug --dvd --dist buster --locale zh_CN --keyboard us  \
+--security-mirror \
+http://mirrors.cloud.tencent.com/debian-security/ \
+--debian-mirror \
+http://mirrors.cloud.tencent.com/debian/  \
+-b amd64-kde.build \
+-a atzlinux-amd64-kde
