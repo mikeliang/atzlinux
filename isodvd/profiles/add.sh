8a6559776825813bd0e55fc81a1d463a41b841ab
xiao sheng wen(肖盛文) <atzlinux@sina.com>
2021-03-29 14:55:05 +0800

    add add.sh

---
+++ isodvd/profiles/add.sh
@@ -0,0 +1,15 @@
+# add packages to all X suite
+
+for f in atzlinux-amd64-xall.packages atzlinux-amd64-xfce.packages atzlinux-amd64-kde.packages atzlinux-all-txt-tech.packages
+	do
+		echo $f;
+		wc -l $f
+		N=`wc -l $f|awk {'print $1'}`
+		echo $N
+		IN=`expr $N - 1`
+
+		sed -i "$IN r to-add" $f
+	done
+
+cat to-add
+wc -l to-add
