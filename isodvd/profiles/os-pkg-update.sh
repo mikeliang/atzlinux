65f909bf4d3a4c71e7fd09881bd608124f11c145
xiao sheng wen(肖盛文) <atzlinux@sina.com>
2022-12-05 11:50:22 +0800

    add os-pkg-update.sh
    Signed-off-by: xiao sheng wen(肖盛文) <atzlinux@sina.com>

---
+++ isodvd/profiles/os-pkg-update.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+for p in `cat os-regular-update.list firmware.pkg.list`
+do
+pooldir=`apt-cache showsrc $p|grep ^Directory:|awk {'print $2'}|head -n 1`
+echo $pooldir
+mkdir -pv $pooldir
+cd $pooldir
+apt-get download $p
+cd -
+done
